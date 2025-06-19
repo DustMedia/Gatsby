2292c9baa44a082d88b6fdb8e38489725353e14f
Roland Branten <roland.branten.partner@decathlon.com>
2025-06-19 09:23:49 +0200

    fix: faulty removeButton logic

--- src/macro-home/Video/Play.js
+++ src/macro-home/Video/Play.js
@@ -46,26 +46,27 @@ export default function Play({ isTouch, performStateChange, setVideoState }) {
 
   return (
     <Wrapper variants={framerPlay} animate="mount" initial="unMount">
-      <motion.svg
-        width="50"
-        viewBox="0 0 58 65"
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.9 }}
-        onTap={onTap}
-        onHoverStart={onHover}
-        initial={{ scale: 1 }}
-        style={{
-          originX: "center",
-          originY: "center",
-          cursor: "pointer",
-          display: removeBtn && "none",
-        }}
-      >
-        <path
-          d="M55.0683 37.5311L8.93881 64.1918C5.024 66.4521 0 63.7153 0 59.1587V5.83725C0 1.28789 5.01675 -1.45617 8.93881 0.811288L55.0683 27.472C55.9588 27.9784 56.6991 28.7103 57.2139 29.5936C57.7288 30.4769 58 31.4802 58 32.5016C58 33.523 57.7288 34.5262 57.2139 35.4095C56.6991 36.2928 55.9588 37.0248 55.0683 37.5311V37.5311Z"
-          fill="#E1C598"
-        />
-      </motion.svg>
+      {!removeBtn && (
+        <motion.svg
+          width="50"
+          viewBox="0 0 58 65"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9 }}
+          onTap={onTap}
+          onHoverStart={onHover}
+          initial={{ scale: 1 }}
+          style={{
+            originX: "center",
+            originY: "center",
+            cursor: "pointer",
+          }}
+        >
+          <path
+            d="M55.0683 37.5311L8.93881 64.1918C5.024 66.4521 0 63.7153 0 59.1587V5.83725C0 1.28789 5.01675 -1.45617 8.93881 0.811288L55.0683 27.472C55.9588 27.9784 56.6991 28.7103 57.2139 29.5936C57.7288 30.4769 58 31.4802 58 32.5016C58 33.523 57.7288 34.5262 57.2139 35.4095C56.6991 36.2928 55.9588 37.0248 55.0683 37.5311V37.5311Z"
+            fill="#E1C598"
+          />
+        </motion.svg>
+      )}
     </Wrapper>
   );
 }
